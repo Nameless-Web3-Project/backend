233afad02a81bcec82269dc2c9203e306717f2cc
Leonardo Lemos <leonardo@sbsistemas.com.br>
2022-05-26 20:38:02 -0300

    refactor: update debug and log

--- src/server/setup-server.ts
+++ src/server/setup-server.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 
-import getPingController from '../controllers/get-ping-controller'
 import loadEnvironment from './load-environment'
+
+import getPingController from '../controllers/get-ping-controller'
+import log from '../log'
 
 function _beforeSetupServer() {
   loadEnvironment()
@@ -20,18 +22,22 @@ function setupServer() {
 
   app.use(bodyParser.json({ limit: '50mb' }))
   app.use(bodyParser.urlencoded({ extended: true }))
+
+  // Morgan is a logger middleware for Express. After it is done right, we'll block
+  // it from production environment.
   app.use(morgan('dev'))
 
   app.get('/ping', getPingController())
 
   const port = process.env.PORT ? Number(process.env.PORT) : 8080
+  const debug = log('http')
 
   app.listen(port, () => {
     // We log it in any environment (either development or production) because the
     // output is certainly useful for debugging purposes, so then we set up a
     // production environment, we'll know whether the server has correctly started
     // or not.
-    console.log(`[server] listening on port ${port}`)
+    debug(`[server] listening on port ${port}`)
 
     _afterSetupServer()
   })
